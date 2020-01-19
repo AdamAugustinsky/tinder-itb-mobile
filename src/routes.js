2ab4f0b483d62a0ba8b963bd6eb25e00e01aa22c
Adam Axel Augustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2020-01-19 20:26:23 -0300

    muda o metodo de navegaçao para createSwitchNavigator

--- src/routes.js
+++ src/routes.js
@@ -1,30 +1,11 @@
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 
-import Cadastro from  './pages/Cadastro';
-import Login from  './pages/Login';
+import Cadastro from  './views/pages/Cadastro';
+import Login from  './views/pages/Login';
 
-const Router = createAppContainer(createStackNavigator({
-  Login: {
-    screen: Login,
-    navigationOptions: {
-      title: 'Login',
-    }
-  },
-  Cadastro: {
-    screen: Cadastro,
-    navigationOptions: {
-      title: 'Cadastro',
-    },
-  },
-},  {
-  defaultNavigationOptions: {
-    headerTintColor: '#fff', 
-    headerStyle: {
-      backgroundColor: '#FF0456'
-    },
-  }
-}));
-
-
-export default Router;
+export default createAppContainer(
+  createSwitchNavigator({
+    Login,
+    Cadastro
+  })
+);
