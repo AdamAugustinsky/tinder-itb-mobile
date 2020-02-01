af019aeae56ed599d3d3a0a19d34ed52421318e9
Adam Axel Augustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2020-02-01 12:07:31 -0300

    add a border to Select

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,14 +1,25 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import { TouchableOpacity, StyleSheet } from 'react-native';
 import RNPickerSelect from 'react-native-picker-select';
 
+const styles = StyleSheet.create({
+  select: {
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: 'rgba(45, 45, 45, 0.32)',
+  },
+});
+
 const Select = ({ state, setState, items }) => (
-  <RNPickerSelect
-    style={{ marginBottom: 10 }}
-    onValueChange={(value) => setState(value)}
-    placeHolder={state}
-    items={items}
-  />
+  <TouchableOpacity style={styles.select}>
+    <RNPickerSelect
+      style={{ marginBottom: 10 }}
+      onValueChange={(value) => setState(value)}
+      placeHolder={state}
+      items={items}
+    />
+  </TouchableOpacity>
 );
 
 export default Select;
