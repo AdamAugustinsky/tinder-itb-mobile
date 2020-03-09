01a92dfdaf7849d9ce827e7d0bbd1eefecf2e15e
Adam Axel Augustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2020-03-09 18:15:02 -0300

    use the deployed api at heroku

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.15.83:3001',
+  baseURL: 'https://tinder-itb-backend.herokuapp.com/',
 });
 
 export default api;
