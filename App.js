e578bba36f13cfc8c1983a163a6c8f008c936a7b
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-03-15 12:07:53 -0300

    feat: carrega fontes personalizadas ao abrir app

--- App.js
+++ App.js
@@ -1,9 +1,26 @@
 /* eslint-disable no-console */
-import React from 'react';
+import * as Font from 'expo-font';
+import React, { useState } from 'react';
 import Router from './src/routes';
 
 console.disableYellowBox = true;
 
-const App = () => <Router />;
+
+const App = () => {
+  const [isReady, setIsReady] = useState(false);
+
+  const loadFonts = async () => {
+    await Font.loadAsync({
+    // eslint-disable-next-line global-require
+      'Poppins-bold': require('./src/assets/fonts/Poppins-Bold.ttf'),
+    });
+
+    setIsReady(true);
+  };
+
+  loadFonts();
+
+  return isReady ? <Router /> : null;
+};
 
 export default App;
