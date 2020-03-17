6164feef7f3f1ab3127bee7dece9af317095a7a1
Adam Axel Augustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2020-03-17 17:12:04 -0300

    fix error when navigating to profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -25,7 +25,7 @@ const styles = StyleSheet.create({
 
 const Profile = ({ navigation }) => {
   const { navigate } = navigation;
-  const [myInformations, setMyInformations] = useState();
+  const [myInformations, setMyInformations] = useState({});
 
   const getJwt = async () => {
     try {
