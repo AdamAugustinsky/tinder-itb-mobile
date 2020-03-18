31733eed5a98f0ae6ef120e84318115839094f09
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-03-18 18:52:00 -0300

    refactor: refatora Select para melhor funcionamento

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,33 +1,45 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { TouchableOpacity, StyleSheet } from 'react-native';
-import RNPickerSelect from 'react-native-picker-select';
+import { StyleSheet, Picker, View } from 'react-native';
 
 const styles = StyleSheet.create({
-  select: {
+  view: {
     position: 'relative',
-    width: 300,
-    height: 40,
-    marginTop: 5,
-    marginBottom: 10,
-    justifyContent: 'center',
-    borderStyle: 'solid',
+    width: '80%',
+    marginTop: 15,
+
     borderWidth: 1,
-    borderColor: 'rgba(45, 45, 45, 0.32)',
+    borderColor: '#2d2d2d',
+    borderRadius: 16,
+  },
+  select: {
+    paddingLeft: 16,
+    height: 46,
   },
 });
 
 const Select = ({
-  state, setState, items, onOpen,
+  enabled, items, setState, state, placeHolder,
 }) => (
-  <TouchableOpacity style={styles.select}>
-    <RNPickerSelect
-      onValueChange={(value) => setState(value)}
-      placeHolder={state}
-      items={items}
-      onOpen={onOpen}
-    />
-  </TouchableOpacity>
+  <View style={styles.view}>
+    <Picker
+      style={styles.select}
+      enabled={enabled}
+      onValueChange={(itemValue, index) => (index === 0 ? null : setState(itemValue))}
+      selectedValue={state}
+      itemStyle={{ fontSize: 33, fontFamily: 'Poppins-bold' }}
+    >
+      <Picker.Item label={placeHolder} value="select" color="#c0c0c0" key="select" />
+      { items.map((item) => (
+        <Picker.Item
+          label={item.label}
+          value={item.value}
+          key={item.value}
+        />
+      ))}
+    </Picker>
+
+  </View>
 );
 
 export default Select;
