6735a42a91bfbf64ad5d399ca03a48a481831a2a
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-03-20 15:30:26 -0300

    feat(models\): adiciona Model User para controle de dados do usuário

---
+++ src/models/User.js
@@ -0,0 +1,19 @@
+class User {
+  constructor() {
+    this.email = '';
+    this.password = '';
+
+    this.name = '';
+    this.gender = '';
+    this.birthdate = '';
+    this.inputBirthdate = '';
+    this.bio = '';
+
+    this.school = '';
+    this.course = '';
+    this.grade = '';
+    this.schoolClass = '';
+  }
+}
+
+export default User;
