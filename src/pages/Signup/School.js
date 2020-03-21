517a81b2b3196a0dcb42bfe18bb306819d58dd9c
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-03-21 12:41:22 -0300

    feat(pages\signup\school): adiciona validação de dados
    já mostrando erros em inputs com a propriedade IsValid

--- src/pages/Signup/School.js
+++ src/pages/Signup/School.js
@@ -1,5 +1,5 @@
 import {
-  KeyboardAvoidingView, Text, BackHandler,
+  KeyboardAvoidingView, Text, BackHandler, Alert,
 } from 'react-native';
 import React, { useState, useEffect } from 'react';
 import Logo from '../../assets/logo.svg';
@@ -7,6 +7,7 @@ import globalStyles from '../../styles/entryStyle';
 import Select from '../../components/Select';
 import api from '../../services/api';
 import BackButton from '../../components/BackButton';
+import Button from '../../components/Button';
 
 const SchoolPage = ({ navigation }) => {
   const { navigate } = navigation;
@@ -15,10 +16,12 @@ const SchoolPage = ({ navigation }) => {
 
   const [school, setSchool] = useState(user.school);
   const [schools, setSchools] = useState([]);
+  const [isSchoolValid, setIsSchoolValid] = useState(true);
 
   const [course, setCourse] = useState(user.course);
   const [courses, setCourses] = useState([]);
-  const [enabled, setEnabled] = useState(false);
+  const [enabled, setEnabled] = useState(!!user.course);
+  const [isCourseValid, setIsCourseValid] = useState(true);
 
   const [grade, setGrade] = useState(user.grade);
   const grades = [
@@ -26,6 +29,7 @@ const SchoolPage = ({ navigation }) => {
     { label: '2º Ano', value: '2' },
     { label: '3º Ano', value: '3' },
   ];
+  const [isGradeValid, setIsGradeValid] = useState(true);
 
   const [schoolClass, setSchoolClass] = useState(user.schoolClass);
   const classes = [
@@ -37,10 +41,47 @@ const SchoolPage = ({ navigation }) => {
     { label: 'F', value: 'F' },
     { label: 'G', value: 'G' },
     { label: 'H', value: 'H' },
+    { label: 'I', value: 'I' },
+    { label: 'J', value: 'J' },
   ];
+  const [isSchoolClassValid, setIsSchoolClassValid] = useState(true);
 
   const [isReady, setIsReady] = useState(false);
 
+
+  const handleNavigation = () => {
+    const title = 'Erro de Validação!';
+
+    if (!school) {
+      setIsSchoolValid(false);
+      return Alert.alert(title, 'Escolha sua escola');
+    }
+    setIsSchoolValid(true);
+    user.school = school;
+
+    if (!course) {
+      setIsCourseValid(false);
+      return Alert.alert(title, 'Escolha seu curso');
+    }
+    setIsCourseValid(true);
+    user.course = course;
+
+    if (!grade) {
+      setIsGradeValid(false);
+      return Alert.alert(title, 'Escolha sua ano');
+    }
+    setIsGradeValid(true);
+    user.grade = grade;
+
+    if (!schoolClass) {
+      setIsSchoolClassValid(false);
+      return Alert.alert(title, 'Escolha sua escola');
+    }
+    setIsSchoolClassValid(true);
+    user.schoolClass = schoolClass;
+
+    return navigate('Contacts', { user });
+  };
 
   const handleBackNavigation = () => navigate('About', { user });
 
@@ -64,6 +105,12 @@ const SchoolPage = ({ navigation }) => {
       })));
 
       setIsReady(true);
+
+      if (course) {
+        const newSchool = school;
+        setSchool('');
+        setSchool(newSchool);
+      }
     }
 
     getSchools();
@@ -110,6 +157,7 @@ const SchoolPage = ({ navigation }) => {
         placeHolder="Escolha sua escola"
         setState={setSchool}
         state={school}
+        isValid={isSchoolValid}
       />
 
       <Select
@@ -118,6 +166,7 @@ const SchoolPage = ({ navigation }) => {
         setState={setCourse}
         state={course}
         enabled={enabled}
+        isValid={isCourseValid}
       />
 
       <Select
@@ -125,6 +174,7 @@ const SchoolPage = ({ navigation }) => {
         placeHolder="Escolha o ano que você estuda"
         setState={setGrade}
         state={grade}
+        isValid={isGradeValid}
       />
 
       <Select
@@ -132,7 +182,10 @@ const SchoolPage = ({ navigation }) => {
         placeHolder="Escolha a sala que você estuda"
         setState={setSchoolClass}
         state={schoolClass}
+        isValid={isSchoolClassValid}
       />
+
+      <Button text="Avançar" onPressed={handleNavigation} />
     </KeyboardAvoidingView>
   ) : null;
 };
