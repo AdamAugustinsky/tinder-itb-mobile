2e1738d0af11070b276b50909f89d5e384e3d770
Adam Axel Augustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2020-03-31 07:47:25 -0300

    fix(ApiController): Pega o jwt antes de usar

--- src/controllers/ApiController.js
+++ src/controllers/ApiController.js
@@ -2,11 +2,16 @@ import LocalStorage from './LocalStorage';
 import api from '../services/api';
 
 export default class ApiController {
+  async getJwt() {
+    this.jwt = await this.Storage.getJwt();
+  }
+
   constructor() {
     this.Storage = new LocalStorage();
   }
 
   async getNewMatch() {
+    await this.getJwt();
     const response = await api.get('/users', { headers: { Authorization: `Bearer ${this.jwt}` } });
     await this.Storage.setMatch(response.data[0]);
     this.match = await this.Storage.getMatch();
