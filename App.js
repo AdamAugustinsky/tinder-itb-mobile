b635522c602b5bc117b4617c344f060f6d62c996
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-04-20 16:16:07 -0300

    clean repository

--- App.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-console */
-import React from 'react';
-import Router from './src/routes';
-
-console.disableYellowBox = true;
-
-const App = () => <Router />;
-
-export default App;
--- src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'https://tinder-itb-backend.herokuapp.com/',
-});
-
-export default api;
