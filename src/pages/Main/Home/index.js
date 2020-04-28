549f70def1c0f9c39f964c1102168d3d21fa07bd
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-04-28 17:51:24 -0300

    refactor(pages/main/home): refaz parte do design da home (estático)

--- src/pages/Main/Home/index.js
+++ src/pages/Main/Home/index.js
@@ -2,99 +2,61 @@
 /* eslint-disable no-underscore-dangle */
 import React, { useState, useEffect } from 'react';
 import {
-  View, Alert, AsyncStorage, ActivityIndicator, Text,
+  Alert,
 } from 'react-native';
 
 
-import styles from './styles';
+import {
+  Container, Title, Background,
+  StyledBar, Group,
+  Info, Name, Image, Row, Age, About,
+} from './styles';
 
 import api from '../../../services/api';
 
-import TargetCard from '../../../components/TargetCard';
 
 import capitalize from '../../../utils/capitalize';
-import ActionButton from '../../../components/ActionButton';
 
 export default function Home() {
   const [user, setUser] = useState();
-  const [users, setUsers] = useState([]);
-  const [index, setIndex] = useState(0);
-  const [haveUsers, setHaveUsers] = useState(true);
 
   useEffect(() => {
-    async function getUsers() {
-      const jwt = await AsyncStorage.getItem('jwt');
+    async function getUser() {
       try {
-        const res = await api.get('/users', {
-          headers: {
-            authorization: `Bearer ${jwt}`,
-          },
-        });
+        const res = await api.get('/users/5e9dca27f4ee9919c0803693');
 
-        setUsers(res.data);
-
-        if (users.length !== 0) { setUser(res.data[index]); } else {
-          setHaveUsers(false);
-          setUser(null);
-        }
+        setUser(res.data);
       } catch (error) {
         Alert.alert('Erro!', capitalize(error.response.data.error));
       }
     }
-    getUsers();
+    getUser();
   }, []);
 
 
-  function changeUser() {
-    setIndex(index + 1);
-
-    if (users[index]) { setUser(users[index]); } else {
-      setHaveUsers(false);
-      setUser(null);
-    }
-  }
-
-  async function like(id) {
-    try {
-      const jwt = await AsyncStorage.getItem('jwt');
-      await api.post(`/profile/likes/${id}`, {}, {
-        headers: {
-          authorization: `Bearer ${jwt}`,
-        },
-      });
-
-      changeUser();
-    } catch (error) {
-      Alert.alert('Erro!', capitalize(error.response.data.error));
-    }
-  }
-  async function dislike(id) {
-    try {
-      const jwt = await AsyncStorage.getItem('jwt');
-      await api.post(`/profile/deslikes/${id}`, {}, {
-        headers: {
-          authorization: `Bearer ${jwt}`,
-        },
-      });
-
-      changeUser();
-    } catch (error) {
-      Alert.alert('Erro!', capitalize(error.response.data.error));
-    }
-  }
-
   return (
-    <View style={styles.container}>
-      {user ? (
-        <View>
-          <TargetCard user={user} />
-          <View style={styles.footer}>
-            <ActionButton type="like" onPress={() => like(user._id)} />
-            <ActionButton type="dislike" onPress={() => dislike(user._id)} />
-          </View>
-        </View>
-      ) : !haveUsers ? <Text>Acabaram os usuários...</Text>
-        : <ActivityIndicator />}
-    </View>
+    <>
+      <Background />
+      <StyledBar />
+      <Container>
+        <Title>Tinder ITB</Title>
+        <Image
+          source={{ uri: user ? user.images[0] : null }}
+          resizeMode="cover"
+        />
+        <Info>
+          <Row>
+            <Group>
+              <Name>{user ? user.name.split(' ')[0] : null}</Name>
+              <Age>
+                {user ? 2020 - new Date(user.birthdate).getFullYear() : null}
+              </Age>
+            </Group>
+            <About />
+          </Row>
+        </Info>
+      </Container>
+
+    </>
   );
 }
--- src/pages/Main/Home/styles.js
+++ src/pages/Main/Home/styles.js
@@ -1,15 +1,82 @@
-import { StyleSheet } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import styled from 'styled-components/native';
+import { StatusBar } from 'react-native';
+import Constants from 'expo-constants';
+import { Feather } from '@expo/vector-icons';
 
-export default StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingHorizontal: 8,
-    paddingVertical: 24,
-    justifyContent: 'space-around',
-  },
-  footer: {
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-  },
-});
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  margin-top: ${Constants.statusBarHeight + 20}px;
+  align-items: center;
+`;
+
+export const Background = styled(LinearGradient).attrs({
+  colors: ['#FF4A86', '#FFA16C'],
+  start: [0.15, 0.2],
+  end: [1, 1],
+})`
+  width: 100%;
+  height: 70%;
+  position: absolute
+`;
+
+export const StyledBar = styled(StatusBar).attrs({
+  backgroundColor: 'rgba(45, 45, 45, 0.3)',
+  translucent: true,
+})``;
+
+export const Title = styled.Text`
+  align-self: center;
+  color: #ffffff;
+  font-family: 'Poppins-bold';
+  font-size: 36px
+`;
+
+export const Info = styled.View`
+  width: 300px;
+  background-color: #fff;
+  padding: 12px;
+  padding-bottom: 32px;
+  borderBottomLeftRadius: 8px
+  borderBottomRightRadius: 8px
+`;
+
+export const Name = styled.Text`
+  color: rgba(11, 37, 88, 0.8);
+  font-size: 24px;
+  line-height: 24px;
+  font-weight: bold;
+  font-family: Roboto
+`;
+
+export const Image = styled.Image`
+  width: 300px; 
+  maxHeight: 330px;
+  height: 100%;
+  borderTopLeftRadius: 8px
+  borderTopRightRadius: 8px
+`;
+
+export const Row = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Age = styled.Text`
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 24px;
+  margin-left: 4px;
+  color: rgba(11, 37, 88, 0.6);
+`;
+
+export const About = styled(Feather).attrs({
+  name: 'info',
+  color: '#BDBDBD',
+  size: 24,
+})``;
+
+export const Group = styled.View`
+  flex-direction: row
+`;
