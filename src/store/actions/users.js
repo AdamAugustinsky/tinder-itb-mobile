2a4cf7518a50bdde25f8255c477d707128429523
AdamAugustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2020-05-25 06:28:04 -0300

    refactor(store/users): setPretender diminue chamadas a API

--- src/store/actions/users.js
+++ src/store/actions/users.js
@@ -3,6 +3,7 @@ import api from '../../services/api';
 
 const Types = {
   ADD_INDEX: '@users/ADD_INDEX',
+  SET_PRETENDER: '@users/SET_PRETENDER',
   GET_PRETENDER: '@users/GET_PRETENDER',
 };
 
@@ -11,19 +12,29 @@ function addIndex() {
   return ({ type: Types.ADD_INDEX });
 }
 
-async function getPretender(jwt) {
+async function setPretender(jwt) {
   try {
     const { data } = await api.get('/users', { headers: { Authorization: `Bearer ${jwt}` } });
-    return ({ type: Types.GET_PRETENDER, pretenders: data });
+    return ({ type: Types.SET_PRETENDER, pretenders: data });
   } catch (error) {
     console.log(error);
-    return ({ type: Types.GET_PRETENDER, pretenders: {} });
+    return ({ type: Types.SET_PRETENDER, pretenders: {} });
+  }
+}
+
+function getPretender() {
+  try {
+    return ({ type: Types.GET_PRETENDER });
+  } catch (error) {
+    console.log(error);
+    return ({ type: Types.GET_PRETENDER });
   }
 }
 
 
 export {
   Types,
+  setPretender,
   getPretender,
   addIndex,
 };
--- src/store/reducers/users.js
+++ src/store/reducers/users.js
@@ -2,16 +2,23 @@ import { Types } from '../actions/users';
 
 const INITIAL_STATE = {
   pretenders: [],
+  pretender: {},
   index: 0,
 };
 
 export default function (state = INITIAL_STATE, action) {
   const { type } = action;
 
-  if (type === Types.GET_PRETENDER) {
+  if (type === Types.SET_PRETENDER) {
     return {
       ...state,
       pretenders: action.pretenders,
+      pretender: action.pretenders[state.index],
+    };
+  } if (type === Types.GET_PRETENDER) {
+    return {
+      ...state,
+      pretender: state.pretenders[state.index],
     };
   } if (type === Types.ADD_INDEX) {
     if (state.index === 5) {
